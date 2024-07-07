ec5ddeaeb6dbc0063cb0a397af6c1d4270ca2777
amirhilal044 <amiramirhilal@gmail.com>
2024-07-07 14:22:31 +0300

    Add client.py

---
+++ client.py
@@ -0,0 +1,18 @@
+import yaml
+import os
+
+class Client:
+    def __init__(self):
+        self.config = self.load_config()
+
+    def load_config(self):
+        config_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config.yaml')
+        with open(config_path, 'r') as file:
+            return yaml.safe_load(file)
+
+    def print_server_ip(self):
+        print(f"Server IP Address: {self.config['ServerIPAddress']}")
+
+if __name__ == "__main__":
+    client = Client()
+    client.print_server_ip()
